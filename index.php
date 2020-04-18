8747b95620fea836936819a29fc46c7b11cfb9b4
Girish Gupta <girish@girish-gupta.com>
2020-04-18 14:48:16 +0530

    Adds getfromsheet

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {
     case '/':
         require 'main.php';
         break;
-    case '/test':
-        require 'test.php';
+    case '/getfromsheet':
+        require 'CSVtoFirestore.php';
         break;
     default:
         http_response_code(404);
